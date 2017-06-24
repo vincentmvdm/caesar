de447d0f8cd0e86abdf2dc593f408b48403de6d8
Vincent M. van der Meulen <meulen@uw.edu>
2017-06-24 15:19:46 -0700

    Add fonts and mute warning

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,5 +1,26 @@
 import { injectGlobal } from 'styled-components';
 
+import circularLight from './fonts/circular-light.otf';
+import circularBold from './fonts/circular-bold.otf';
+
+/* eslint no-unused-expressions: 0 */
 injectGlobal`
-    
+    @font-face {
+        font-family: Circular;
+        src: url(${circularLight});
+        font-weight: 200;
+        font-style: normal;
+    }
+
+    @font-face {
+        font-family: Circular;
+        src: url(${circularBold});
+        font-weight: 500;
+        font-style: normal;
+    }
+
+    body {
+        font-family: Circular;
+        font-weight: 200;
+    }
 `;
