9d56de168ccf4b7d49c640efce5bd2d0ea5e1a9e
Vincent M. van der Meulen <meulen@uw.edu>
2017-06-24 14:36:35 -0700

    Add wrapping div

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         return (
-            caesar
+            <div>
+                Caesar
+            </div>
         );
     }
 }
