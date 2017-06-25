ac198f6813e7c20354484b8a75dc4b9a7d8dbb23
Aishwarya Mandyam <aishrm2@uw.edu>
2017-06-24 17:55:53 -0700

    finished server merging stuff

---
+++ server/public/public/groups.js
@@ -0,0 +1,25 @@
+var groupsJSON = require('./groups');
+
+function createGroup(user_id){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+	groupsJSON.push({key: genGroupCode(), value: [user_id]});
+}
+
+function genGroupCode(){
+	high = 9999
+	low = 1111
+	return Math.random() * (high - low) + low
+}
+
+function addUser(group_code, user_id){
+	groupsJSON.push({key:group_code, value:groupsJSON[group_code].push(user_id)})
+}
+
+function getGroupUsers(group_code, user_id){
+	if(group_code in groupsJSON){
+		if user_id in groupsJSON[group_code]:
+			return groupsJSON[group_code]
+		else:
+			return "You are not in this group"
+	}
+	return "Invalid Group Code"
+}
