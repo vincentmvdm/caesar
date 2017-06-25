109c869c6ba515463b6cba6ce222cabf31d1e498
Christian Ng <christianr.ng@gmail.com>
2017-06-24 21:26:18 -0700

    rewrote groups as mutators library

--- server/groups.js
+++ server/groups.js
@@ -1,7 +1,7 @@
-var groupsJSON = require('./groups');
-
-function createGroup(user_id){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
-	groupsJSON.push({key: genGroupCode(), value: [user_id]});
+//these functions modify a dictionary which should be hosted in core server
+function createGroup(user_id, groupsJSON, top_tracks){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+	groupsJSON.push({key: genGroupCode(), value: [user_id, top_tracks]});
+	return groupsJSON
 }
 
 function genGroupCode(){
@@ -10,16 +10,14 @@ function genGroupCode(){
 	return Math.random() * (high - low) + low
 }
 
-function addUser(group_code, user_id){
-	groupsJSON.push({key:group_code, value:groupsJSON[group_code].push(user_id)})
+function addUser(group_code, user_id, groupsJSON, top_tracks){
+	groupsJSON.push({key:group_code, value:groupsJSON[group_code].push(user_id).push(top_tracks)})
+	return groupsJSON
 }
 
-function getGroupUsers(group_code, user_id){
+function getGroup(group_code, groupsJSON){
 	if(group_code in groupsJSON){
-		if user_id in groupsJSON[group_code]:
-			return groupsJSON[group_code]
-		else:
-			return "You are not in this group"
+		return groupsJSON[group_code]
 	}
 	return "Invalid Group Code"
 }
